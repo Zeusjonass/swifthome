7be45eb4c8959eeb3ab96b4682c6c8d292e79e57
Osmar <osmarcc70@gmail.com>
2025-07-03 17:35:52 -0600

    detalles seo

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -14,6 +14,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import Head from 'next/head';
 
 const drawerWidth = 294;
 const headerHeight = 96;
--- src/admin/components/layout/Header.tsx
+++ src/admin/components/layout/Header.tsx
@@ -1,24 +1,26 @@
 import { AppBar, Toolbar, Box, IconButton, Avatar, Typography } from "@mui/material";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import Image from "next/image";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import Link from "next/link";
 
 export const Header = ({headerHeight}: {headerHeight: number}) => {
-  //TODO: Cambiar a Link y path
+  const { user } = useAuthenticator((context) => [context.user]);
   return (
     <AppBar position="sticky" sx={{ height: `${headerHeight}px`, boxShadow: 'none', borderBottom: '2px solid #E2E1E1', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar sx={{ backgroundColor: 'white', height: '100%' }}>
         <Box sx={{ flexGrow: 1 }}>
-          <a href="#" style={{display: 'inline-block'}}>
+          <Link href="/chat" style={{display: 'inline-block'}}>
             <Image src="/fostwjj399sm1ozl1e9-1-2.png.webp" alt="Logo" width={200} height={47.72} />
-          </a>
+          </Link>
         </Box>
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <IconButton color="inherit" >
             <NotificationsIcon fontSize="medium" sx={{ color: 'action.active'}} />
           </IconButton>
-          <Avatar alt="User" src="/" sx={{ width: 30, height: 30, mr: '8px' }} />
+          <Avatar alt={user.username} src="/" sx={{ width: 30, height: 30, mx: '8px' }} />
           <Typography variant="body1" sx={{ color: "black", fontSize: 'small' }}>
-            User name
+            {user.username}
           </Typography>
         </Box>
       </Toolbar>
